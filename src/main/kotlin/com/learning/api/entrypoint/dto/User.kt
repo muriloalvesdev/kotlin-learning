64acfa88deb9617dd514d1f5fd8997044a4f2449
muriloalvesdev <murilo.alves@picpay.com>
2022-11-03 22:35:12 -0300

    more improvings

--- src/main/kotlin/com/learning/api/entrypoint/dto/User.kt
+++ src/main/kotlin/com/learning/api/entrypoint/dto/User.kt
@@ -3,12 +3,8 @@ package com.learning.api.entrypoint.dto
 import com.learning.api.dataprovider.database.entity.UserEntity
 
 data class User(
-    private val username: String
+    val username: String
 ) {
-    fun username(): String {
-        return this.username
-    }
-
     companion object {
         @JvmStatic
         fun build(entity: UserEntity): User {
