4abb3a5ee320aae232b422d625235cf10d58047d
muriloalvesdev <murilo.alves@picpay.com>
2022-11-03 22:35:32 -0300

    utils for tests

--- src/test/kotlin/com/learning/api/utils/ConstantsTests.kt
+++ src/test/kotlin/com/learning/api/utils/ConstantsTests.kt
@@ -1,4 +1,8 @@
 package com.learning.api.utils
 
 class ConstantsTests {
+    companion object {
+        const val PROFILE_TEST: String = "test"
+        const val USERNAME_TEST: String = "muriloalvesdev"
+    }
 }
